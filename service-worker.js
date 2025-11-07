98b26edf89d84d693c066427977a75f976a529c5
angelcalvijo2505-creator <angelcalvijo2505@gmail.com>
2025-11-07 18:34:39 -0500

    Add basic service worker for fetch handling
    Implement basic service worker for PWA caching.

---
+++ service-worker.js
@@ -0,0 +1,8 @@
+// Un service worker básico para PWA
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
